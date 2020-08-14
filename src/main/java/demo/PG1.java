edb1e15be7aa0b0a54f30690ad282eadfd188f14
rakeshyadav <rakesh.yadav@gmail.com>
2020-08-14 13:07:08 -0400

    demo

--- src/main/java/demo/PG1.java
+++ src/main/java/demo/PG1.java
@@ -1,12 +1,11 @@
 package demo;
 
-import org.testng.annotations.Test;
 //comment the above line and uncomment below line to use Chrome
 //import org.openqa.selenium.chrome.ChromeDriver;
 public class PG1 {
 
 
-	@Test
+	
     public  void test() {
         // declaration and instantiation of objects/variables
     	System.out.println("Testing");
