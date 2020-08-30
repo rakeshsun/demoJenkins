c71dadc50ad0386903ff0f387a6c404fa65bbb26
rakeshsun <rakesh.ya82@gmail.com>
2020-08-30 19:12:52 -0400

    Update demo-pipeline.groovy

--- jenkins-pipeline/demo-pipeline.groovy
+++ jenkins-pipeline/demo-pipeline.groovy
@@ -1,48 +1,13 @@
 
     node () {
 
-        def isBuildFailure = false
-        //def gridURL = "${env.GRIDLASTIC_URL}"
-        def application = 'bsd'
-        def browser = 'chrome'
-        def testName = 'CatalogTest_BSD'
-        def environment = 'PROD'
-        def repoName = 'prod'
-        def testUrl = 'https://business.officedepot.com'
-        def testDataFile = 'TestData-Prod.xml'
-        def testngFile= 'testng-specialprojects.xml'
-        def repo = 'https://github.com/officedepot/bsd-automation-reloaded.git'
-        def PARAMS
-        def emailList = 'qa-automation-team@officedepot.com'
-        def constants
-        def gitId
-        def svnId
-        def testngFilesList
-        def testdataFilesList
-        def wwwTestUrl
-        def packageName = 'SpecialProjects'
-        def mavenContainerVersion
-        def dataProviderFileList
-        def testngFilePath = 'resources/testng/'
-        def testngFailedXml
-
-        //ReportPortal Props
-        def rpEndpoint
-        def rpUuid
-        def rpProject
-        def rpLaunch
-        def rpTags
-        def rpDescription
+       
         stage("Read Constants and Shared Library") {
             script {
                 constants = evaluate readTrusted('jenkins_pipeline/pipeline_constants.groovy')
                 sharedLib = evaluate readTrusted('jenkins_pipeline/common_lib.groovy')
                 gitId = "${constants.gitId}"
-                testngFilesList = "${constants.testngFilesList}"
-                testdataFilesList = "${constants.testdataFilesList}"
-                svnId = "${constants.svnId}"
-                wwwTestUrl = "${constants.wwwTestUrl}"
-                mavenContainerVersion = "${constants.mavenContainerVersion}"
+            
             }
         }
 
